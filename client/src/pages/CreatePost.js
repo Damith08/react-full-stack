4323cd2429ae90f0148e031216b4e9ebde6acb49
Damith08 <dmthsamarasinghe@gmail.com>
2024-01-23 13:09:16 +0530

    feat: create form

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,5 +1,33 @@
 import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 export default function CreatePost() {
-  return <div>CreatePost</div>;
+  return (
+    <div className="createPostPage">
+      <Formik>
+        <Form>
+          <label>Title:</label>
+          <Field
+            id="inputCreatePost"
+            name="title"
+            placeholder="(Ex. Title...)"
+          />
+          <label>Posts:</label>
+          <Field
+            id="inputCreatePost"
+            name="postsText"
+            placeholder="(Ex. Post...)"
+          />
+          <label>Username:</label>
+          <Field
+            id="inputCreatePost"
+            name="username"
+            placeholder="(Ex. John123)"
+          />
+
+          <button type="submit">Create Post</button>
+        </Form>
+      </Formik>
+    </div>
+  );
 }
