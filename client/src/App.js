9e536e3750a3c1d89eef1b2d9a3675839e6d9220
Damith08 <dmthsamarasinghe@gmail.com>
2024-01-23 12:18:33 +0530

    feat: connecting react to nodejs

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [listOfPosts, setListOfPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/posts").then((res) => {
+      setListOfPosts(res.data);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      {listOfPosts.map((value, key) => {
+        return (
+          <div className="post" key={value.id}>
+            <div className="title">{value.title}</div>
+            <div className="body">{value.postsText}</div>
+            <div className="footer">{value.username}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
