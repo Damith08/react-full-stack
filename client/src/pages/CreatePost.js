da9a655dc154c8bc80089d7ddd6a3334b2c0ab43
Damith08 <dmthsamarasinghe@gmail.com>
2024-01-23 13:15:14 +0530

    feat: add styles to the form

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -5,21 +5,24 @@ export default function CreatePost() {
   return (
     <div className="createPostPage">
       <Formik>
-        <Form>
+        <Form className="formContainer">
           <label>Title:</label>
           <Field
+            autocomplete="off"
             id="inputCreatePost"
             name="title"
             placeholder="(Ex. Title...)"
           />
           <label>Posts:</label>
           <Field
+            autocomplete="off"
             id="inputCreatePost"
             name="postsText"
             placeholder="(Ex. Post...)"
           />
           <label>Username:</label>
           <Field
+            autocomplete="off"
             id="inputCreatePost"
             name="username"
             placeholder="(Ex. John123)"
